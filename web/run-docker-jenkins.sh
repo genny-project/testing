ed43c9c5132a3cdc288df1db2b194dc6b1e18448
DomHynes <DominicHynes@gmail.com>
2018-01-12 12:47:24 +1100

    added docker run for jenkins

---
+++ web/run-docker-jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -z "${1}" ]; then
+   version="latest"
+else
+   version="${1}"
+fi
+if [ -z "${2}" ]; then
+   WEB_URL="http://v2.channel40.com.au"
+   #WEB_URL="http://channel40-web-dev.channel40.com.au:81"
+else
+   WEB_URL="${2}"
+fi
+echo "WEB_URL="$WEB_URL
+docker run -t --rm -e WEB_URL=${WEB_URL} -v $PWD/reports:/home/robot-framework-logs -v $PWD/screenshots:/home/robot-framework/screenshots gennyproject/robot:${version} 
