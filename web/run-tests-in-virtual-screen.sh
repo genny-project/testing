13490783c9ab0efe6172b3b636d8fc902fcecf3c
DomHynes <DominicHynes@gmail.com>
2018-01-25 14:43:09 +1100

    fixed script path

--- web/run-tests-in-virtual-screen.sh
+++ web/run-tests-in-virtual-screen.sh
@@ -2,4 +2,4 @@
 
 xvfb-run --server-args="-screen 0 ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_COLOUR_DEPTH} -ac" robot --outputDir /opt/robotframework/reports /opt/robotframework/tests
 
-results_parse.py /opt/robotframework/reports/results.xml ${SLACK_WEBHOOK}
+results-parse.py /opt/robotframework/reports/results.xml ${SLACK_WEBHOOK}
