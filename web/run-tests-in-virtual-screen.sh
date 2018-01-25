d2171beb5bb4725610060b3481dfe9464eac26e5
DomHynes <DominicHynes@gmail.com>
2018-01-25 14:46:30 +1100

    actually fixed the path

--- web/run-tests-in-virtual-screen.sh
+++ web/run-tests-in-virtual-screen.sh
@@ -2,4 +2,4 @@
 
 xvfb-run --server-args="-screen 0 ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_COLOUR_DEPTH} -ac" robot --outputDir /opt/robotframework/reports /opt/robotframework/tests
 
-results-parse.py /opt/robotframework/reports/results.xml ${SLACK_WEBHOOK}
+results-parse.py /opt/robotframework/reports/output.xml ${SLACK_WEBHOOK}
