682edc4bdcd76cc66e6b065aca099f60dfd4fff4
DomHynes <DominicHynes@gmail.com>
2018-02-14 11:26:40 +1100

    fixed testing

--- web/run-docker-dev.sh
+++ web/run-docker-dev.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
-PWD=`pwd`
+# PWD=`pwd`
+# if [ -z "${1}" ]; then
+#    WEB_URL="http://localhost:5000"
+# else
+#    WEB_URL="${1}"
+# fi
+# if [ -z "${2}" ]; then
+#    KEYCLOAKURL="http://localhost:8180/auth/admin"
+# else
+#    KEYCLOAKURL="${2}"
+# fi
+
 if [ -z "${1}" ]; then
-   WEB_URL="http://v2.channel40.com.au"
+   WEB_URL="https://v2.channel40.com.au"
 else
    WEB_URL="${1}"
 fi
 if [ -z "${2}" ]; then
-   KEYCLOAKURL="http://localhost:8180/auth/admin"
+   KEYCLOAK_URL="https://bouncer.outcome-hub.com/auth/admin"
 else
-   KEYCLOAKURL="${2}"
+   KEYCLOAK_URL="${2}"
 fi
 
 echo "testing ${WEB_URL}"
@@ -21,5 +32,5 @@ docker run --rm -d --name robot-dev  \
   -v $PWD/tests:/opt/robotframework/tests  \
   -v $PWD/reports:/opt/robotframework/reports  \
   gennyproject/robot:latest
-  
+
 docker logs -f robot-dev
--- web/tests/Smoke_Test/01_WF_USERREGISTER.robot
+++ web/tests/Smoke_Test/01_WF_USERREGISTER.robot
@@ -21,13 +21,14 @@ ${screenshot_name}  WF_USERREGISTER{index}.png
    WF_DR_AUTH_REDIRECT
    #WF_DR_REGISTER
 
-01_WF_USERREGISTER_KEYCLOAK_ADMIN
+02_WF_USERREGISTER_KEYCLOAK_ADMIN
    [Tags]  Register
    WF_DR_AUTH_KEYCLOAK_ADMIN
 
 *** Keywords ***
 INITIALIZE_SETUP
    Initialize System
+
    ${status}=  run keyword and return status  Directory Should Exist  ${screenshot_path}
    Log  ${status}
    run keyword if  '${status}' == 'True'  Empty Directory  ${screenshot_path}
