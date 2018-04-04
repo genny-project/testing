056d66ed6c6609aac48a94aa627a709a9a596194
DomHynes <DominicHynes@gmail.com>
2018-04-04 13:07:37 +1000

    preparing for jenkins testing

--- web/commands.sh
+++ web/commands.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-
 today=`date '+%Y_%m_%d__%H_%M_%S'`;
 
 $(x11vnc -rfbport 4500 -rfbauth /tmp/vnc/pass -display :90 -forever -auth /tmp/vnc/xvfb.auth) &
 
-<./stop ffmpeg -f x11grab -video_size 1920x1080 -i :90 -c:v libx264 -preset ultrafast -r 12 /opt/robotframework/reports/$today.mkv &
+
+
+if [[ -z "${WAIT_FOR_IT}" ]]; then
+  echo "Waiting 30s"
+  sleep 30s
+fi
+
+
+ffmpeg -f x11grab -video_size 1920x1080 -i :90 -c:v libx264 -preset ultrafast -r 12 /opt/robotframework/reports/$today.mkv &
 
 DISPLAY=":90" robot --outputDir /opt/robotframework/reports /opt/robotframework/tests
-
-echo 'q' > stop
