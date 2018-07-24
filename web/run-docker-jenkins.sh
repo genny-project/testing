00f1e8fd80b9c15a96c23ddaf6460d98e9f26c1c
Adam Crow <acrow@Adams-Compooter.local>
2018-07-24 14:16:05 +1000

    Cleaned up

--- web/run-docker-jenkins.sh
+++ web/run-docker-jenkins.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 if [ -z "${1}" ]; then
-   version="latest"
+   url="http://alyson.genny.life"
 else
-   version="${1}"
+   url="${1}"
 fi
-if [ -z "${2}" ]; then
-   WEB_URL="http://v2.channel40.com.au"
-   #WEB_URL="http://channel40-web-dev.channel40.com.au:81"
-else
-   WEB_URL="${2}"
-fi
-echo "WEB_URL="$WEB_URL
 echo "reports="$PWD/reports
-docker run --rm -e WEB_URL=http://v2.channel40.com.au -e BROWSER=chrome -v $PWD/reports:/opt/robotframework/reports -v $PWD/screenshots:/home/robotframework/screenshots gennyproject/robot:latest
+docker run --rm -e WEB_URL=${url} -e BROWSER=chrome -v $PWD/reports:/opt/robotframework/reports -v $PWD/screenshots:/home/robotframework/screenshots gennyproject/robot:latest
--- web/run/docker/smokeTest.sh
+++ web/run/docker/smokeTest.sh
@@ -3,4 +3,4 @@
 cd ../../
 PWD=`pwd`
 
-docker run -e WEB_URL=http://v2.channel40.com.au/ -v $PWD/tests/smokeTest:/home/robot-framework/tests/smokeTest -v $PWD/reports:/home/robot-framework-logs -v $PWD/resources:/home/robot-framework/resources -v $PWD/screenshots:/home/robot-framework/screenshots rohitcs/rohit-robot
+docker run -e WEB_URL=${1}/ -v $PWD/tests/smokeTest:/home/robot-framework/tests/smokeTest -v $PWD/reports:/home/robot-framework-logs -v $PWD/resources:/home/robot-framework/resources -v $PWD/screenshots:/home/robot-framework/screenshots rohitcs/rohit-robot
